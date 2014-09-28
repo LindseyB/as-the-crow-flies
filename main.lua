7f6bc6d833d25c8788873992aac1720f1b506e5a
Lindsey Bieda <hazardous.narayan@gmail.com>
2014-09-28 16:11:16 -0400

    Copying and pasting some SAT code.

--- main.lua
+++ main.lua
@@ -138,10 +138,10 @@ end
 
 
 function colliding_check(x1,y1,w1,h1,x2,y2,w2,h2)
-  return x1 < x2+w2 and
-		 x2 < x1+w1 and
-		 y1 < y2+h2 and
-		 y2 < y1+h1
+	return x1 < x2+w2 and
+		x2 < x1+w1 and
+		y1 < y2+h2 and
+		y2 < y1+h1
 end
 
 function reset()
